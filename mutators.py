c6eee2c2399fa9203cad3211d0b4b0a58ed451e4
Michal Melewski <carstein@google.com>
2013-08-12 10:45:39 +0200

    Followup on name change

---
+++ mutators.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import re
+import itertools
+
+MATCH_RESPONSE=1
+MATCH_REQUEST=2
+
+class MutatorFrame:
+    match=""
+    
+    def __init__(self, match):
+        raise NotImplementedError
+    
+    def set_match(self,match):
+        self.match = match
+    
+    def check_match(self,req):
+        if match != "":
+            m=re.search(self.match,req)
+            if m and m.group(0):
+                return True
+            else:
+                return False
+        else:
+            return True
+    
+    def mutate(self, data=""):
+        return date
+        
+class SimpleMutator(MutatorFrame):
+    payload=[]
+    gen = None
+    
+    def __init__(self):
+        pass
+    
+    def add_payload(self,payload):
+        self.payload.append(payload)
+        self.gen = itertools.cycle(self.payload)
+    
+    def mutate(self, data=""):
+            return self.gen.next()
+
+class SwapMutator(MutatorFrame):
+    def __init__(self):
+        pass
+        
+    def mutate(self, data=""):
+        return data.swapcase()
+
+class OverflowMutator(MutatorFrame):
+    mul = 128
+    
+    def __init__(self):
+        pass
+
+    def mutate(self, data=""):
+        self.mul*=2
+        return "A"*self.mul
